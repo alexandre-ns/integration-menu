285967e4a61e9a5f67cb5a81cc1f27c00c4c2dda
alexandre <alexandren49@gmail.com>
2024-08-07 17:52:48 -0300

    app anotai

---
+++ anotai/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class AnotaiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'anotai'
+    verbose_name = "PLATAFORMA ANOTA AI"
